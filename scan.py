c90d7111c1ef043d17061621db666b20d0a787af
Bhanu <124144755+Bhanu6576@users.noreply.github.com>
2024-03-06 17:41:56 +0530

    Update scan.py

--- scan.py
+++ scan.py
@@ -247,8 +247,8 @@ class Spinner:
                     print(x,end='')
                     if inc>random.uniform(0,terminal_size()): #30 init
                         print(end="\r")
-                        bcolors.BG_SCAN_TXT_START = '\x1b[6;30;'+str(round(random.uniform(46,47)))+'m'
-                        print(Loading)
+                        bcolors.BG_SCAN_TXT_START = '\x1b[6;30;'+str(round(random.uniform(47)))+'m'
+                        
                         inc = 0
                     sys.stdout.flush()
                 time.sleep(self.delay)
